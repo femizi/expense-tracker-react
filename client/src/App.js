f9154cd6ff0f199aef0c8a674c145b8b68b763d1
Femi Efejuku <obito.femi@gmail.com>
2021-10-18 12:24:22 +0100

    fixing issue

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Balance } from './components/Balance'
 import {IncomeExpenses} from './components/IncomeExpenses'
 import {TransactionList} from './components/TransactionList'
 import {AddTransaction} from './components/AddTransaction'
-import {GlobalContext, GlobalProvider} from './context/GlobalState'
+import { GlobalProvider} from './context/GlobalState'
 
 
 function App() {
